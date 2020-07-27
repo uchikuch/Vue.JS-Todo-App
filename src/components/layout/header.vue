284a3fd2a41c0a48c0d269a9d2a3a37e6184dd0c
uchikuch <ucheazinge@gmail.com>
2020-07-27 09:30:59 +0100

    first commit -v1 complete

---
+++ src/components/layout/header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header">
+    <h1>TodoList</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+.header {
+  background: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+}
+
+.header a {
+  color: #fff;
+  padding-right: 5px;
+  text-decoration: none;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #fff;
+}
+
+#nav a.router-link-exact-active {
+  color: #91cff8;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>Uche Azinge's TodoList app v1.0.0. Used to get a feel for Vue.JS</p>
+  </div>
+</template>
